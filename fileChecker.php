76cfbec29bfc548710d1e3466808518aadbcf2e5
JamesRea83 <jamesrea83@gmail.com>
2017-04-12 07:57:04 +0100

    front & back end files added. Not currently functioning.

---
+++ fileChecker.php
@@ -0,0 +1,20 @@
+<?php
+
+$path = $_POST["mypath"];
+
+function timeStamper($path) {
+
+	$size = filesize($path);
+	 
+
+	$returnObj->size = $size;
+	$returnJSON = json_encode($returnObj, JSON_UNESCAPED_SLASHES);
+	   
+	return $returnJSON;
+   }
+
+
+echo timeStamper($path);
+  
+ 
+?>
